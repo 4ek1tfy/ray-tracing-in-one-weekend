98ccb102b5e3995da131265de23265a7ba3684b9
4ek1tfy <stas22der@gmail.com>
2026-08-16 19:54:02 +0300

    fix: vec3; feat: color

--- include/vec3.hpp
+++ include/vec3.hpp
@@ -4,9 +4,9 @@
 #include <iostream>
 
 class vec3{
-public:
+private:
     double e[3];
-    
+public:    
     vec3() : e{0,0,0} {}
 
     vec3(double e0, double e1, double e2) : e{e0, e1, e2} {}
@@ -51,23 +51,23 @@ public:
 using point3 = vec3;
 
 inline std::ostream& operator<<(std::ostream& out, const vec3& v){
-    return out << v.e[0] << ' ' << v.e[1] << ' ' << v.e[2];
+    return out << v.x() << ' ' << v.y() << ' ' << v.z();
 }
 
 inline vec3 operator+(const vec3& v1, const vec3& v2){
-    return vec3(v1[0] + v2[0], v1[1] + v2[1], v1[2] + v2[2]);
+    return vec3(v1.x() + v2.x(), v1.y() + v2.y(), v1.z() + v2.z());
 }
 
 inline vec3 operator-(const vec3& v1, const vec3& v2){
-    return vec3(v1[0] - v2[0], v1[1] - v2[1], v1[2] - v2[2]);
+    return vec3(v1.x() - v2.x(), v1.y() - v2.y(), v1.z() - v2.z());
 }
 
 inline vec3 operator*(const vec3& v1, const vec3& v2){
-    return vec3(v1[0] * v2[0], v1[1] * v2[1], v1[2] * v2[2]);
+    return vec3(v1.x() * v2.x(), v1.y() * v2.y(), v1.z() * v2.z());
 }
 
 inline vec3 operator*(const double& t, const vec3& v){
-    return vec3(t * v[0], t * v[1], t * v[2]);
+    return vec3(t * v.x(), t * v.y(), t * v.z());
 }
 
 inline vec3 operator*(const vec3& v, const double& t){
@@ -79,13 +79,13 @@ inline vec3 operator/(const vec3& v, const double& t){
 }
 
 inline double dot(const vec3& v1, const vec3& v2){
-    return v1[0] * v2[0] + v1[1] * v2[1] + v1[2] * v2[2];
+    return v1.x() * v2.x() + v1.y() * v2.y() + v1.z() * v2.z();
 }
 
 inline vec3 cross(const vec3& v1, const vec3& v2){
-    return vec3(v1[1] * v2[2] - v1[2] * v2[1],
-                v1[2] * v2[0] - v1[0] * v2[2],
-                v1[0] * v2[1] - v1[1] * v2[0]);
+    return vec3(v1.y() * v2.z() - v1.z() * v2.y(),
+                v1.z() * v2.x() - v1.x() * v2.z(),
+                v1.x() * v2.y() - v1.y() * v2.x());
 }
 
 inline vec3 unit_vector(const vec3& v){
