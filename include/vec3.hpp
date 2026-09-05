deba02beb5009f4498aa396fbda3e6a5a6e8ac13
4ek1tfy <stas22der@gmail.com>
2026-09-05 10:52:22 +0300

    feat: Positionable Camera

--- include/vec3.hpp
+++ include/vec3.hpp
@@ -12,7 +12,7 @@ public:
     double y() const {return e[1];}
     double z() const {return e[2];}
 
-    vec3 operator-() {return vec3(-e[0], -e[1], -e[2]);}
+    vec3 operator-() const {return vec3(-e[0], -e[1], -e[2]);}
     double operator[](int i) const {return e[i];}
     double& operator[](int i) {return e[i];}
 
@@ -126,4 +126,11 @@ inline vec3 random_on_hemisphere(const vec3& normal){
 
 inline vec3 reflect(const vec3& v, const vec3& n) {
     return v - 2 * dot(v, n)*n;
+}
+
+inline vec3 refract(const vec3& uv, const vec3& n, double etai_over_etat) {
+    auto cos_theta = std::fmin(dot(-uv, n), 1.0);
+    vec3 r_out_perp =  etai_over_etat * (uv + cos_theta*n);
+    vec3 r_out_parallel = -std::sqrt(std::fabs(1.0 - r_out_perp.length_squared())) * n;
+    return r_out_perp + r_out_parallel;
 }
